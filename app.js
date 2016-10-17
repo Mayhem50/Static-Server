c91d26782b2cb9a5fd4aeb6471f62259c0e3989b
Benjamin REGNIER <benjamin.regnier@lumiscaphe.com>
2016-10-17 14:24:11 +0200

    _ Project Creation

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var path = require('path');
+
+var app = express();
+// static file serve
+app.use(express.static(path.join(__dirname, 'public')));
+// not found in static files, so default to index.html
+app.use((req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+module.exports = app;
